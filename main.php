7a1df632654bb5872e8388836e6b87d6a11ac996
Adrien DEVAL <adrien.deval@ikmail.com>
2025-02-05 14:38:15 +0100

    ...

--- main.php
+++ main.php
@@ -5,19 +5,40 @@ require 'vendor/autoload.php';
 use Monolog\Level;
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
-use PHPMailer\PHPMailer\PHPMailer;
 use Symfony\Component\Dotenv\Dotenv;
 
-function addFighter($l) {
-    // TODO : Ajouter le code pour insérer un combattant dans la base de données
+function addFighter($l, $DATABASE_HOST, $DATABASE_NAME, $DATABASE_USERNAME, $DATABASE_PASSWORD, $DATABASE_PORT, $DATABASE_DIALECT)
+{
 
-    $l->info('The fighter is added');
+    try {
+        $dbh = new PDO('$DATABASE_DIALECT:host=$DATABASE_HOST;dbname=test', $DATABASE_USERNAME, $DATABASE_PASSWORD);
+
+        // TODO: Add a fighter to the database
+
+        $l->info('Connection has been established');
+    } catch (PDOException $e) {
+        $l->error('Connection failed: ' . $e->getMessage());
+    }
+
+    $l->info('The fighter has been added to the database');
 }
+
+// Premiers pas avec les requêtes préparées
+print("Premiers pas avec les requêtes préparées\n");
 
 $log = new Logger('ApplicationSIO');
 $log->pushHandler(new StreamHandler('log/info.log', Level::Warning));
 
 $dotenv = new Dotenv();
-$dotenv->load(__DIR__.'/.env');
+$dotenv->load(__DIR__ . '/.env');
 
-addFighter($l);
+// add a fighter
+addFighter(
+    $log,
+    $_ENV['DATABASE_HOST'],
+    $_ENV['DATABASE_NAME'],
+    $_ENV['DATABASE_USERNAME'],
+    $_ENV['DATABASE_PASSWORD'],
+    $_ENV['DATABASE_PORT'],
+    $_ENV['DATABASE_DIALECT']
+);
