b7a57a79d4355247df3af6718981cb0c508688dd
Adrien DEVAL <adrien.deval@ikmail.com>
2025-02-05 15:30:42 +0100

    Requêtes préparées

--- main.php
+++ main.php
@@ -10,35 +10,59 @@ use Symfony\Component\Dotenv\Dotenv;
 function addFighter($l, $DATABASE_HOST, $DATABASE_NAME, $DATABASE_USERNAME, $DATABASE_PASSWORD, $DATABASE_PORT, $DATABASE_DIALECT)
 {
     try {
-        $l->info('Trying to connect to the database $DATABASE_NAME on $DATABASE_HOST:$DATABASE_PORT with $DATABASE_DIALECT dialect');
-        $dbh = new PDO('$DATABASE_DIALECT:host=$DATABASE_HOST;dbname=test', $DATABASE_USERNAME, $DATABASE_PASSWORD);
+        $l->info("Trying to connect to the database $DATABASE_NAME on $DATABASE_HOST:$DATABASE_PORT with $DATABASE_DIALECT dialect");
+        $dbh = new PDO("$DATABASE_DIALECT:host=$DATABASE_HOST;dbname=$DATABASE_NAME", $DATABASE_USERNAME, $DATABASE_PASSWORD);
 
-        // TODO: Add a fighter to the database
+        ////////////////////////////////////////////////////////////////////////////////////
+
+        // Préparation de la requête
+        $stmt = $dbh->prepare("INSERT INTO fighters (name, strength, defense) VALUES (:name, :strength, :defense)");
+
+        //Déclaration des variables
+        $name = "Miss Fortune";
+        $strength = 100;
+        $defense = 50;
+
+        // Liaison des paramètres
+        $stmt->bindParam(':name', $name);
+        $stmt->bindParam(':strength', $strength);
+        $stmt->bindParam(':defense', $defense);
+
+        // Exécution de la requête
+        $stmt->execute();
+
+        ////////////////////////////////////////////////////////////////////////////////////
 
         $l->info('Connection has been established');
     } catch (\PDOException $e) {
         $l->error('Connection failed: ' . $e->getMessage());
     }
 
-    $l->info('The fighter has been added to the database');
+    // $l->info('The fighter has been added to the database');
+}
+
+function checkStrength($strength) {
+    if($strength < 0) {
+        throw new Exception('La force du guerrier ne peut pas être négative');
+    }
 }
 
 // Premiers pas avec les requêtes préparées
-print("Premiers pas avec les requêtes préparées\n");
+print("Premiers pas avec la gestion des erreurs (Exceptions)\n");
 
 $log = new Logger('ApplicationSIO');
-$log->pushHandler(new StreamHandler('log/info.log', Level::Warning));
+$log->pushHandler(new StreamHandler('log/info.log', Level::Debug));
 
 $dotenv = new Dotenv();
 $dotenv->load(__DIR__ . '/.env');
 
-// add a fighter
-addFighter(
-    $log,
-    $_ENV['DATABASE_HOST'],
-    $_ENV['DATABASE_NAME'],
-    $_ENV['DATABASE_USERNAME'],
-    $_ENV['DATABASE_PASSWORD'],
-    $_ENV['DATABASE_PORT'],
-    $_ENV['DATABASE_DIALECT']
-);
+// addFighter($log, $_ENV['DATABASE_HOST'], $_ENV['DATABASE_NAME'], $_ENV['DATABASE_USERNAME'], $_ENV['DATABASE_PASSWORD'], $_ENV['DATABASE_PORT'], $_ENV['DATABASE_DIALECT']);
+
+try {
+    checkStrength(-5);
+} catch (\Throwable $th) {
+    $log->error($e->getMessage());
+    echo "Erreur ! $th->getMessage()\n\n";
+}
+
+print("Le programme continue...\n");
