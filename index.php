dd2163b977e2c2a8f4f622713b66c8cc0f8dd6d3
Adrien DEVAL <adrien.deval@ikmail.com>
2025-02-06 11:13:19 +0100

    Modifications mineures

--- index.php
+++ index.php
@@ -4,6 +4,10 @@ $couleur = "#FFFFFF"; // Couleur par défaut (blanc)
 if (isset($_COOKIE["couleur_preferee"])) {
     $couleur = $_COOKIE["couleur_preferee"];
 }
+
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["couleur"])) {
+    setcookie("couleur_preferee", $_POST["couleur"], time() + 3600);
+}
 ?>
 
 <!DOCTYPE html>
@@ -19,7 +23,12 @@ if (isset($_COOKIE["couleur_preferee"])) {
 
 <body>
     <h1>Bienvenue sur notre site !</h1>
-    <p><a href="set_color.php">Choisir une autre couleur</a></p>
 </body>
+
+<form method="post" action="index.php">
+    <label for="couleur">Choisissez votre couleur préférée :</label>
+    <input type="color" id="couleur" name="couleur" value="<?php echo $couleur; ?>">
+    <button type="submit">Valider</button>
+</form>
 
 </html>
--- set_color.php
+++
@@ -1,6 +0,0 @@
-<form method="post">
-    <label for="couleur">Choisissez votre couleur préférée :</label>
-    <input type="color" id="couleur" name="couleur">
-    <button type="submit">Valider</button>
-</form>
-
